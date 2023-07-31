81c75d117f8baecdbf2da84ed790ff2849a4e22e
egilman2 <etg4@rice.edu>
2023-07-31 14:23:22 -0700

    more utility functions for similarity of embeddings

--- SmallTanimoto.py
+++ SmallTanimoto.py
@@ -47,15 +47,62 @@ def score_test_smiles(training_smiles, test_smiles):
         trainer_map[smile], score_map[smile] = find_most_similar_training_mol(training_smiles, smile)
     return trainer_map, score_map
 
+def pairwise_tanimoto_similarity(smiles):
+    """
+    Takes a list of smiles of length i and computes pairwise tanimoto similarity for all of them
+    :param smiles: a list of smiles
+    :return: a dictionary where key (i,j) maps to the tanimoto similarity between the smile at index i and
+    the smile at index j
+    """
+    mfpgen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=2048)
+    mols = [Chem.MolFromSmiles(smile) for smile in smiles]
+    fingies = [mfpgen.GetFingerprint(mol) for mol in mols]
+    results = {}
+    for i in range(len(smiles)):
+        for j in range(i, len(smiles)):
+            results[(i, j)] = Chem.DataStructs.TanimotoSimilarity(fingies[i], fingies[j])
+            if i != j:
+                results[(j, i)] = results[(i, j)]
+    return results
 
-# rand_smiles = ['CC(c1ccccc1)N1CC[C@H]1[C@@H](N)c1cccc(Cl)c1', 'N[C@@H](c1ccc(Cl)cc1)[C@@H]1CCN1C(c1ccccc1)c1ccccc1',
-#                'CCNC(=O)c1ccc(C(=C2CC3CCC(C2)N3c2cccn2C)c2ccccc2)cc1',
-#                'CC(C)C[C@H](NC(=O)[C@H](Cc1ccccc1)NC(=O)[C@@H]1CCCN1C(=O)CNC(=O)[C@@H](N)Cc1ccc(O)cc1)C(N)=O']
+def pairwise_euclidean_distance(embeddings):
+    """
+    Takes a 2D numpy array and computes the pairwise euclidean distance between the row vectors
+    :param embeddings: a 2D numpy array
+    :return: a dictionary where the key (i,j) maps to the Euclidean distance between the ith and jth
+    row vectors of the embeddings array
+    """
+    results = {}
+    for i in range(len(embeddings)):
+        for j in range(i, len(embeddings)):
+            results[(i, j)] = np.linalg.norm(embeddings[i] - embeddings[j])
+            if i != j:
+                results[(j, i)] = results[(i, j)]
+    return results
+
+def pairwise_cosine_similarity(embeddings):
+    """
+    Takes a 2D numpy array and computes the pairwise cosine similarity between the row vectors
+    :param embeddings: a 2D numpy array
+    :return: a dictionary where the key (i,j) maps to the cosine similarity between the ith and jth
+    row vectors of the embeddings array
+    """
+    results = {}
+    for i in range(len(embeddings)):
+        for j in range(i, len(embeddings)):
+            results[(i, j)] = np.dot(embeddings[i], embeddings[j])/(np.linalg.norm(embeddings[i]) * np.linalg.norm(embeddings[j]))
+            if i != j:
+                results[(j, i)] = results[(i, j)]
+    return results
+
+rand_smiles = ['CC(c1ccccc1)N1CC[C@H]1[C@@H](N)c1cccc(Cl)c1', 'N[C@@H](c1ccc(Cl)cc1)[C@@H]1CCN1C(c1ccccc1)c1ccccc1',
+               'CCNC(=O)c1ccc(C(=C2CC3CCC(C2)N3c2cccn2C)c2ccccc2)cc1',
+               'CC(C)C[C@H](NC(=O)[C@H](Cc1ccccc1)NC(=O)[C@@H]1CCCN1C(=O)CNC(=O)[C@@H](N)Cc1ccc(O)cc1)C(N)=O']
 #
 # rand_tester = 'CCNC(=O)c1ccc(C(=C2CC3CCC(C2)N3CCO)c2ccccc2)cc1'
 #
 # print(find_most_similar_training_mol(rand_smiles, rand_tester))
-
+#print(pairwise_cosine_similarity(rand_smiles).items())
 # train_smis = list(pd.read_csv('219_prep/train.csv')['smiles'])
 # test_smis = list(pd.read_csv('219_prep/test.csv')['smiles'])
 # results = score_test_smiles(train_smis, test_smis)
